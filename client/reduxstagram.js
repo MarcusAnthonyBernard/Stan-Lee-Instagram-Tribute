35fb5332aec76e4abfd3f9e381f1ce9a982a4fea
marcusbass323 <marcus@bassconsultantgroup.com>
2018-11-26 20:41:31 -0800

    INIT

---
+++ client/reduxstagram.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import css from './styles/style.styl';
+import Main from './components/Main'
+
+
+
+render(
+    <Main />, 
+
+
+document.getElementById('root'));
+    
